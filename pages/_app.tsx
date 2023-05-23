b9228d8f717de491cf1daedbc45a319f5460d237
Roman Dąbal <dabalroman@gmail.com>
2023-05-24 01:31:05 +0200

    QC23-22: FontAwesome;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@ import type { AppProps } from 'next/app';
 import { UserContext } from '@/lib/context';
 import { useUserData } from '@/lib/hooks';
 import { Toaster } from 'react-hot-toast';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 export default function App ({
     Component,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import kebabCase from 'lodash.kebabcase';
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGem } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home () {
     return (
@@ -12,6 +14,7 @@ export default function Home () {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <main>
+                <FontAwesomeIcon icon={faGem}/>
                 <h1 className="text-3xl font-bold underline">{kebabCase('Hello world!')}</h1>
                 <Link href={'enter'}>Enter</Link>
             </main>
