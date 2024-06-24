bb41c343e0b2018edd2914a73d722cb47849b5f2
Roman Dąbal <dabalroman@gmail.com>
2024-06-25 00:15:59 +0200

    3D card with dynamic glare;

--- README.md
+++ README.md
@@ -24,6 +24,7 @@ npm run dev
 
 ## Emulator
 ```bash
+npm install -g firebase-tools
 npm run emulators
 ```
 
--- components/CardComponent.tsx
+++ components/CardComponent.tsx
@@ -2,15 +2,57 @@ import Card from '@/models/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDiceD6 } from '@fortawesome/free-solid-svg-icons';
 import { getCardTierFriendlyName } from '@/Enum/CardTier';
+import { useEffect, useRef, useState } from 'react';
 
 export default function CardComponent ({
     card,
     className = ''
 }: { card: Card, className?: string }) {
+    const cardRef = useRef(null);
+    const [initialOrientation, setInitialOrientation] = useState({
+        beta: 0,
+        gamma: 0
+    });
+
+    useEffect(() => {
+        const handleOrientation = (event: DeviceOrientationEvent) => {
+            if (!cardRef.current) {
+                return;
+            }
+
+            if (initialOrientation.beta === 0 && initialOrientation.gamma === 0) {
+                setInitialOrientation({
+                    beta: event.beta ?? 0,
+                    gamma: event.gamma ?? 0
+                });
+            }
+
+            const xTilt = event.gamma ?? 0 - initialOrientation.gamma; // range [-90,90]
+            const yTilt = event.beta ?? 0 - initialOrientation.beta; // range [-180,180]
+
+            const maxTilt = 45;
+
+            const xRotation = -((xTilt + 90) / 180 - 0.5) * maxTilt;
+            const yRotation = ((yTilt + 180) / 360 - 0.5) * maxTilt * 2;
+
+            cardRef.current.style.transform =
+                `perspective(600px) rotateX(${yRotation}deg) rotateY(${xRotation}deg) scale(0.9)`;
+            cardRef.current.style.setProperty('--glare-left', `${xRotation / 10 * 50 - 50}%`);
+        };
+
+        window.addEventListener('deviceorientation', handleOrientation);
+
+        return () => {
+            window.removeEventListener('deviceorientation', handleOrientation);
+        };
+    }, [initialOrientation]);
+
     return (
         <div
+            ref={cardRef}
             className={
-                'border-8 border-card-border rounded-3xl bg-background relative bg-center bg-cover'
+                'rounded-3xl bg-background relative bg-center bg-cover '
+                + 'ring-8 ring-inset ring-card-border shadow-card-border duration-150 ease-in-out'
                 + ' ' + className
             }
             style={{
@@ -18,22 +60,40 @@ export default function CardComponent ({
                 'minHeight': '200px',
                 'maxHeight': '70vh',
                 'aspectRatio': '2/3',
-                'objectFit': 'contain'
+                'objectFit': 'contain',
+                '--glare-left': '-50%',
+                'overflow': 'hidden',
+                'transformStyle': 'preserve-3d',
+                'transform': 'scale(0.9)'
             }}
         >
             <div
-                className="bg-card-border text-center text-text-accent absolute top-0 right-0 pb-2 pl-4 pt-0 pr-2
+                className="bg-card-border text-center text-text-accent absolute top-0 right-0 pb-4 pl-5 pt-2 pr-4
                 rounded-bl-3xl"
             >
-                <span className="text-2xl font-fancy-capitals block">
+                <span className="text-4xl font-fancy-capitals block">
                     <FontAwesomeIcon icon={faDiceD6} size="xs" className="relative top-1"/> {card.value}
                 </span>
-                <span className="mt-1 block">{getCardTierFriendlyName(card.tier)}</span>
+                <span className="mt-1 block text-xl">{getCardTierFriendlyName(card.tier)}</span>
             </div>
             <div
-                className="bg-card-border text-text-accent absolute bottom-0 left-0 pb-2 pl-2 pt-3 pr-4 rounded-tr-3xl">
-                <span className="text-2xl font-fancy-capitals">{card.name}</span>
+                className="bg-card-border text-text-accent absolute bottom-0 left-0 pb-4 pl-4 pt-3 pr-4 rounded-tr-3xl">
+                <span className="text-4xl font-fancy-capitals">{card.name}</span>
             </div>
+            <div
+                className="glare"
+                style={{
+                    'position': 'absolute',
+                    'top': '0',
+                    'left': 'var(--glare-left)',
+                    'width': '700px',
+                    'height': '100%',
+                    'background': 'linear-gradient(70deg, rgba(255,255,255,0) 30%, rgba(255,255,255,0.25) 45%, '
+                        + 'rgba(255,255,255,0.3) 50%, rgba(255,255,255,0.25) 55%, rgba(255,255,255,0) 70%)',
+                    'pointerEvents': 'none',
+                    'zIndex': '10'
+                }}
+            ></div>
         </div>
     );
 }
