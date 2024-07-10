eee9d4bdf1886299df93c2a498377f9b14639e05
Roman Dąbal <dabalroman@gmail.com>
2024-07-10 22:21:40 +0200

    Card collections seeds;

--- functions/src/seeds/cardSetsSeed.ts
+++ functions/src/seeds/cardSetsSeed.ts
@@ -3,58 +3,46 @@ import { CardSet } from '../types/cardSet';
 
 const cardSetsSeed: CardSet[] = [
     {
-        uid: 'magic-creatures',
-        amountOfCards: 8,
-        name: 'Magiczne stworzenia',
-        description: 'Te niezwykłe istoty, posiadające nadprzyrodzone moce i umiejętności, były mi znane tylko z opowieści i legend, ale teraz mam je przed sobą.'
-    },
-    {
-        uid: 'steam-city',
-        amountOfCards: 6,
-        name: 'Miasto pary',
-        description: 'Moją podróż zakończyłem w cudownym miaście, gdzie zetknąłem się z niesamowitymi mechanizmami i wszechobecną parą. Stalowe konstrukcje pulsują życiem, tworząc mistyczną symfonię industrii i marzeń.'
-    },
-    {
-        uid: 'darkest-night',
-        amountOfCards: 6,
-        name: 'Mroki nocy',
-        description: 'W tęsknotę za światłem zaklęte, moje mroczne wspomnienia wyłaniają się spod powierzchni. W cieniach ukazują się potwory, a widma złożone z lęku wplatają się w krajobrazy, mrożące krew w żyłach.'
-    },
-    {
-        uid: 'from-the-past',
-        amountOfCards: 5,
-        name: 'Początki',
-        description: 'To pięć kart, pięć różnych ścieżek, które z dzieciństwa prowadziły do bohaterskich legend. Każda z nich to dowód na to, że wielkość zaczyna się małością.'
-    },
-    {
-        uid: 'postcards-from-far-away',
-        amountOfCards: 5,
-        name: 'Pocztówki z daleka',
-        description: 'Widzę wielkie góry w majestatycznym pięknie, świątynię otoczoną mistycznym blaskiem i średniowieczne miasto, gdzie czas zastygł w kamiennych uliczkach. Podróż w jednym spojrzeniu.'
-    },
-    {
-        uid: 'nebula',
-        amountOfCards: 5,
-        name: 'Nebula',
-        description: 'W mgławicowym tańcu moich wspomnień odsłaniają się postacie światła, kolorów i tęczy. Ich mieniące się barwy i neonowe wzornictwo tworzą magiczną symfonię dla oczu.'
-    },
-    {
-        uid: 'heroes',
+        uid: 'gildia-pustyni',
         amountOfCards: 9,
-        name: 'Znajomi z podróży',
-        description: 'Portrety bohaterów spotkanych w mej wędrówce ożywają na kartach tej kolekcji. Ich twarze przenikają serce i umysł, niosąc ze sobą historie pełne niesamowitych przygód. To w ich towarzystwie uczyniłem krok ku nieznanemu.'
+        name: 'Gildia Pustyni',
+        description: '"Siedzieliśmy cicho w cieniu wydm, gdy słońce zaczęło zachodzić za horyzont. Szejk kazał nam czekać na zmrok, kiedy straże Gildii Stali miały być najmniej czujne. Zebraliśmy się w małych grupach i ruszyliśmy w kierunku tamy."'
     },
     {
-        uid: 'galaxy',
-        amountOfCards: 6,
-        name: 'Galaktyczne sny',
-        description: 'Kolekcja ta to podróż przez ukryte zakątki wszechświata. Tajemnice Azury-9, stalowe bramy New Theta, monumentalny Ziggurat - to jedyne takie opowieści, które trzymam w swoich rękach.'
+        uid: 'gildia-mroku',
+        amountOfCards: 8,
+        name: 'Gildia Mroku',
+        description: '"Zamek Cienia był przerażający nawet z daleka. Kiedyś tam byłem i czułem, jak zimno przenika mnie do kości. Nawet teraz, gdy leży w gruzach, wciąż czuję jego mroczną obecność. To miejsce nigdy nie przestanie mnie straszyć."'
     },
     {
-        uid: 'secret',
-        amountOfCards: 0,
-        name: 'Tajemnica',
-        description: 'Ta tajna kolekcja zostanie poznana tylko przez szczególnie dociekliwych.'
+        uid: 'gildia-stali',
+        amountOfCards: 9,
+        name: 'Gildia Stali',
+        description: '"Kolejny nieudany atak pustynnych barbarzyńców na tamę Nahr\'al" - przeczytał nagłówek z oburzeniem. Zacisnął szczęki, patrząc na towarzyszy przy stole. "Znowu próbowali. Myślą, że zdołają zniszczyć nasze dzieło, nasz cud inżynierii".'
+    },
+    {
+        uid: 'gildia-wody',
+        amountOfCards: 13,
+        name: 'Gildia Wody',
+        description: 'Woda dookoła Żarłacza wrzała od wybuchów, a ciemne niebo przeszywały błyskawice. Nikt nie spodziewał się, że morskie stworzenia przyjdą nam z pomocą - widziałem na własne oczy, jak Gryfony otaczały wrogie statki. Dzięki ich pomocy odnieśliśmy zwycięstwo, ale do portu wróciła tylko połowa naszej floty.'
+    },
+    {
+        uid: 'fauna',
+        amountOfCards: 13,
+        name: 'Fauna',
+        description: '"Absolutnie fascynujące" - szepnęła Maren, zbliżając się do stołu badawczego. "Zastanawiam się, jak dokładnie działa ich bioluminescencja. Czy to kwestia symbiozy z bakteriami, czy może mają specjalne organelle odpowiedzialne za produkcję światła?"'
+    },
+    {
+        uid: 'kraina-erindar',
+        amountOfCards: 8,
+        name: 'Kraina Erindar',
+        description: 'Rozkaz był prosty - "Zebrać jak najwięcej". Gdy Thorin zaczął ostrożnie odłupywać kryształy usłyszeliśmy huk, a wejście do jaskini zasłoniła ogromna, lodowata sylwetka Szronokła. Potężny jaszczur syczał złowieszczo, blokując jedyną drogę ucieczki. Byliśmy uwięzieni.'
+    },
+    {
+        uid: 'gildie',
+        amountOfCards: 4,
+        name: 'Gildie',
+        description: '"Widziałem, jak z chaosu powstały cztery gildie, każda z potrzeby i ambicji, każda snująca inne sny o przyszłości. A my, mieszkańcy Erindaru, jesteśmy świadkami tej niekończącej się gry o władzę."'
     }
 ];
 
